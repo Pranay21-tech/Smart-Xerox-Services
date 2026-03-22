6939818aa237ac71c7253ea7c7db8d415a151357
Shekapuram Pranay <shekapurampranay@gmail.com>
2026-03-22 11:39:27 +0530

    fix template path for render

--- smartproject/backend/backend/settings.py
+++ smartproject/backend/backend/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
 {
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [BASE_DIR / 'templates'],
+    'DIRS': [os.path.join(BASE_DIR, 'smartxerox', 'templates')],  # 🔥 FIX
     'APP_DIRS': True,
     'OPTIONS': {
         'context_processors': [
